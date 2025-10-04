4d3981d39a9deebf4e92aab3262e67a29abdc434
Haydarov Diyor <alyreniko@gmail.com>
2025-10-04 13:13:32 +0700

    feat: switch from Vite to Nuxt
    Switched to Nuxt support for SSG support

---
+++ app/components/NavLinks.vue
@@ -0,0 +1,26 @@
+<script setup>
+import links from "assets/links.json";
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li v-for="(l, i) in links" :key="i">
+                <a
+                    class="link-card"
+                    :href="l.url"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <span class="link-icon" aria-hidden="true"
+                        ><Icon :name="l.icon"
+                    /></span>
+                    <span class="link-text">
+                        <span class="link-title">{{ l.title }}</span>
+                        <span class="link-sub">{{ l.subtitle }}</span>
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default {
+  target: "static",
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/styles.css"],
+  modules: ["@nuxt/icon"],
+};
