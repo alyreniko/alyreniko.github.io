9efe5d532d80c1d30ff4991b3cc387c4254e19e1
Haydarov Diyor <alyreniko@gmail.com>
2025-10-11 21:42:22 +0700

    feat(header): add component, global styles and logo

---
+++ app/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="flex justify-center items-center gap-8 fixed top-4 w-screen">
+    <a class="btn px-6 py-3 rounded-xl header__link" href="/blog">Blog</a>
+    <a class="btn px-6 py-3 rounded-xl header__link" href="#projects">Projects</a>
+    <a href="/"><img class="btn px-6 py-2 rounded-xl header__link" src="/logo.svg" alt="logo" /></a>
+    <a class="btn px-6 py-3 rounded-xl header__link" href="#about">About me</a>
+    <a class="btn px-6 py-3 rounded-xl header__link" href="#contact">Contact</a>
+  </header>
+</template>
+
+<style>
+.header__link:hover {
+  color: var(--color-accent-secondary);
+  background-color: var(--color-accent-primary);
+  border-color: var(--color-accent-secondary);
+  &:active {
+    color: var(--color-accent-primary);
+    background-color: var(--color-accent-secondary);
+    border-color: var(--color-accent-primary);
+  }
+}
+</style>
