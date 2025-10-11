1339e598027c91cfcf84c1b127a7d5b94aae60d1
Haydarov Diyor <alyreniko@gmail.com>
2025-10-11 14:13:36 +0700

    chore: set up ESLint and Prettier with integration

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+export default {
+  singleQuote: true, // Использовать одинарные кавычки
+  semi: true, // Точки с запятой в конце строк
+  trailingComma: 'es5', // Запятые после последнего элемента в объектах/массивах
+  printWidth: 100, // Максимальная длина строки
+  tabWidth: 2, // Отступ 2 пробела
+  endOfLine: 'auto', // Автоопределение перевода строк (Windows/Linux)
+  htmlWhitespaceSensitivity: 'ignore', // Не ломать верстку <template> в Vue
+};
