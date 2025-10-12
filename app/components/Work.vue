9b100998875700ccd0b7367c710499b2fbdae38b
Haydarov Diyor <alyreniko@gmail.com>
2025-10-12 17:40:06 +0700

    feat(work): add design projects
    - Separate work items into 'codeWork' and 'designWork' data structures -
    Render two distinct sections with different layout and aspect ratios -
    Code projects retain tags and descriptions; design projects show only
    title and source link - Update image aspect ratio: 2:1 for code, 1:1 for
    design

--- app/components/Work.vue
+++ app/components/Work.vue
@@ -1,28 +1,45 @@
 <template>
-  <div class="flex min-h-screen gap-8">
-    <div
-      v-for="(item, key) in work"
-      :key="key"
-      class="flex flex-col code__card block-layer2 bg-neutral-800 max-w-[420px] h-fit gap-4"
-    >
-      <img draggable="false" class="aspect-[2/1] object-cover rounded-lg" :src="item.image" />
-      <div class="text-neutral-200">
-        <h3 class="text-lg mb-2 text-neutral-100">{{ key }}</h3>
-        <p class="text-base text-neutral-400">{{ item.description }}</p>
-      </div>
-      <div class="flex flex-nowrap justify-between items-end">
-        <div class="code__tags flex gap-3 self-end flex-wrap">
-          <span
-            v-for="tag in item.tags"
-            :key="tag"
-            class="py-1 px-2 rounded-lg w-fit h-fit text-neutral-400 bg-neutral-700 text-sm"
-          >
-            {{ tag }}
-          </span>
+  <div class="flex flex-col min-h-screen gap-52 py-32">
+    <div class="flex gap-8">
+      <div
+        v-for="(item, key) in codeWork"
+        :key="key"
+        class="flex flex-col code__card block-layer2 bg-neutral-800 max-w-[420px] h-fit gap-4"
+      >
+        <img draggable="false" class="aspect-[2/1] object-cover rounded-lg" :src="item.image" />
+        <div class="text-neutral-200">
+          <h3 class="text-lg mb-2 text-neutral-100 font-medium">{{ key }}</h3>
+          <p class="text-base text-neutral-400">{{ item.description }}</p>
         </div>
-        <a href="" class="btn p-1 rounded-xl">
-          <Icon :name="iconMap[item.source]" class="flex w-14 h-14" />
-        </a>
+        <div class="flex flex-nowrap justify-between items-end">
+          <div class="code__tags flex gap-3 self-end flex-wrap">
+            <span
+              v-for="tag in item.tags"
+              :key="tag"
+              class="py-1 px-2 rounded-lg w-fit h-fit text-neutral-400 bg-neutral-700 text-sm"
+            >
+              {{ tag }}
+            </span>
+          </div>
+          <a href="" class="btn p-1 rounded-xl">
+            <Icon :name="iconMap[item.source]" class="flex w-14 h-14" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="flex gap-8">
+      <div
+        v-for="(item, key) in designWork"
+        :key="key"
+        class="flex flex-col code__card block-layer2 bg-neutral-800 max-w-[420px] h-fit gap-4"
+      >
+        <img draggable="false" class="aspect-[1/1] object-cover rounded-lg" :src="item.image" />
+        <div class="flex flex-nowrap justify-between items-center">
+          <h3 class="text-lg mb-2 text-neutral-100 font-medium">{{ key }}</h3>
+          <a href="" class="btn p-1 rounded-xl">
+            <Icon :name="iconMap[item.source]" class="flex w-14 h-14" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -34,7 +51,7 @@ const iconMap = {
   behance: 'streamline-logos:behance-logo-block',
 };
 
-const work = {
+const codeWork = {
   'Photo2Pixel Dithering': {
     image: 'https://dummyimage.com/1200x600/444/aaa',
     description:
@@ -48,8 +65,20 @@ const work = {
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nemo deleniti deserunt voluptatibus, accusamus minus a dolorem tempora, reiciendis libero labore quisquam temporibus dolores. Unde cum nulla fugiat praesentium sit?',
     tags: ['lorem', 'lorem', 'lorem'],
-    source: 'behance',
+    source: 'github',
     url: 'https://github.com/alyreniko/pong_pygame',
+  },
+};
+const designWork = {
+  Design: {
+    image: 'https://dummyimage.com/1200x1200/444/aaa',
+    source: 'behance',
+    url: 'https://behance.net/',
+  },
+  Lorem: {
+    image: 'https://dummyimage.com/1200x1200/444/aaa',
+    source: 'behance',
+    url: 'https://behance.net/',
   },
 };
 </script>
