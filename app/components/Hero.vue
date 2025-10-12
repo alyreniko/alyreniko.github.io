54ed557e75e0c74913423197ae9c502a20af65c6
Haydarov Diyor <alyreniko@gmail.com>
2025-10-12 14:24:35 +0700

    style(layout): replace hero margin with body padding for consistent spacing

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex mx-[280px] gap-10 overflow-x-hidden">
+  <div class="flex gap-10 overflow-x-hidden min-h-screen items-center justify-center">
     <div class="block-layer1 flex flex-col gap-5">
       <div class="block-layer2 inline-flex gap-5">
         <div class="inline-table">
