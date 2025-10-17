aba45334b97d2c80e77d0e4b5f916414b1c941b4
Haydarov Diyor <alyreniko@gmail.com>
2025-10-17 18:03:47 +0700

    feat(footer): add Footer component (#16)

---
+++ app/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="*:text-neutral-600 a:hover:text-neutral-400 text-center py-2 bg-neutral-900">
+    <p>© 2025 Diyor Haydarov (alyreniko). All rights reserved.</p>
+    <p>
+      The site is an open source project - repository on
+      <a class="link" href="">GitHub</a>
+    </p>
+    <p class="flex justify-center gap-1">
+      Follow me on social media:
+      <a class="link" href="">LinkedIn</a>
+      |
+      <a class="link" href="">GitHub</a>
+      |
+      <a class="link" href="">Behance</a>
+      |
+      <a class="link" href="">Pinterest</a>
+    </p>
+    <p class="flex justify-center gap-1">
+      Quick links:
+      <a class="link" href="">About_me</a>
+      |
+      <a class="link" href="">Portfolio</a>
+      |
+      <a class="link" href="">Blog</a>
+      |
+      <a class="link" href="">Contact</a>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.link {
+  transition: all 0.4s ease-in-out;
+}
+
+.link:hover {
+  @apply text-neutral-400;
+}
+</style>
