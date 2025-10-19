241bf2a6a365cb78fe09a9eed81058cfda83db06
Haydarov Diyor <alyreniko@gmail.com>
2025-10-19 18:01:04 +0700

    feat: add works.json and refactor Work.vue

--- app/components/Work.vue
+++ app/components/Work.vue
@@ -5,13 +5,13 @@
       <RunningLine class="-z-50" word="CODE" />
       <div class="block-section grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-8 !mt-4">
         <div
-          v-for="(item, key) in codeWork"
-          :key="key"
+          v-for="(item, i) in codeWork"
+          :key="i"
           class="flex flex-col code__card block-layer2 bg-neutral-800 w-full h-fit gap-4"
         >
           <img draggable="false" class="aspect-[2/1] object-cover rounded-lg" :src="item.image" />
           <div class="text-neutral-200">
-            <h3 class="text-lg mb-2 text-neutral-100 font-medium">{{ key }}</h3>
+            <h3 class="text-lg mb-2 text-neutral-100 font-medium">{{ item.title }}</h3>
             <p class="text-base text-neutral-400">{{ item.description }}</p>
           </div>
           <div class="flex flex-nowrap justify-between items-end">
@@ -24,95 +24,50 @@
                 {{ tag }}
               </span>
             </div>
-            <a href="" class="btn p-1 rounded-xl">
+            <a :href="item.url" class="btn p-1 rounded-xl" target="_blank">
               <Icon :name="iconMap[item.source]" class="flex w-14 h-14" />
             </a>
           </div>
         </div>
       </div>
     </div>
-    <div class="relative">
-      <!-- DESIGN SECTION -->
-      <RunningLine class="-z-50" word="DESIGN" />
-      <div class="block-section grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-8 !mt-4">
-        <div
-          v-for="(item, key) in designWork"
-          :key="key"
-          class="flex flex-col code__card block-layer2 bg-neutral-800 w-full h-fit gap-4"
-        >
-          <img draggable="false" class="aspect-[1/1] object-cover rounded-lg" :src="item.image" />
-          <div class="flex flex-nowrap justify-between items-center">
-            <h3 class="text-lg mb-2 text-neutral-100 font-medium">{{ key }}</h3>
-            <a href="" class="btn p-1 rounded-xl">
-              <Icon :name="iconMap[item.source]" class="flex w-14 h-14" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!-- <div class="relative"> -->
+    <!-- DESIGN SECTION -->
+    <!-- <RunningLine class="-z-50" word="DESIGN" /> -->
+    <!-- <div class="block-section grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-8 !mt-4"> -->
+    <!-- <div -->
+    <!-- v-for="(item, i) in designItems" -->
+    <!-- :key="i" -->
+    <!-- class="flex flex-col code__card block-layer2 bg-neutral-800 w-full h-fit gap-4" -->
+    <!-- > -->
+    <!-- <img -->
+    <!-- draggable="false" -->
+    <!-- class="aspect-[1/1] object-cover rounded-lg" -->
+    <!-- :src="item.image" -->
+    <!-- :alt="item.title" -->
+    <!-- /> -->
+    <!-- <div class="flex flex-nowrap justify-between items-center"> -->
+    <!-- <h3 class="text-lg mb-2 text-neutral-100 font-medium">{{ item.title }}</h3> -->
+    <!-- <a :href="item.url" class="btn p-1 rounded-xl"> -->
+    <!-- <Icon :name="iconMap[item.source]" class="flex w-14 h-14" /> -->
+    <!-- </a> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- </div> -->
   </div>
 </template>
 
 <!-- SCRIPTS -->
-<script setup>
+<script lang="ts" setup>
+import works from '~/assets/works.json';
+
+const designItems = works.design;
+
 const iconMap = {
   github: 'streamline-logos:github-logo-1-block',
   behance: 'streamline-logos:behance-logo-block',
 };
 
-const codeWork = {
-  'Photo2Pixel Dithering': {
-    image: 'https://dummyimage.com/1200x600/444/aaa',
-    description:
-      'Pong in Python using This project is a Pygame - a simple and fun implementation of the classic arcade game for two players. This project is a great way to learn the basics of game great two development.',
-    tags: ['python', 'pygame'],
-    source: 'github',
-    url: 'https://github.com/alyreniko/pong_pygame',
-  },
-  'Ullamco aliqua': {
-    image: 'https://dummyimage.com/1200x600/444/aaa',
-    description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, exercitationem mollitia ducimus voluptates qui esse totam debitis ex officiis fuga quia tenetur libero expedita molestiae iste sequi quae incidunt possimus!',
-    tags: ['python', 'pygame'],
-    source: 'github',
-    url: 'https://github.com/alyreniko/pong_pygame',
-  },
-  'Lorem exercitation': {
-    image: 'https://dummyimage.com/1200x600/444/aaa',
-    description:
-      'Lorem ipsum dolor sit amet, deserunt exercitation veniam esse nostrud sed aute aliqua ad cupidatat sunt',
-    tags: ['python', 'pygame'],
-    source: 'github',
-    url: 'https://github.com/alyreniko/pong_pygame',
-  },
-  'Commodo ad': {
-    image: 'https://dummyimage.com/1200x600/444/aaa',
-    description:
-      'Pong in Python using This project is a Pygame - a simple and fun implementation of the classic arcade game for two players. This project is a great way to learn the basics of game great two development.',
-    tags: ['python', 'pygame'],
-    source: 'github',
-    url: 'https://github.com/alyreniko/pong_pygame',
-  },
-  Lorem: {
-    image: 'https://dummyimage.com/1200x600/444/aaa',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nemo deleniti deserunt voluptatibus, accusamus minus a dolorem tempora, reiciendis libero labore quisquam temporibus dolores. Unde cum nulla fugiat praesentium sit?',
-    tags: ['lorem', 'lorem', 'lorem'],
-    source: 'github',
-    url: 'https://github.com/alyreniko/pong_pygame',
-  },
-};
-
-const designWork = {
-  Design: {
-    image: 'https://dummyimage.com/1200x1200/444/aaa',
-    source: 'behance',
-    url: 'https://behance.net/',
-  },
-  Lorem: {
-    image: 'https://dummyimage.com/1200x1200/444/aaa',
-    source: 'behance',
-    url: 'https://behance.net/',
-  },
-};
+const codeWork = works.code;
 </script>
